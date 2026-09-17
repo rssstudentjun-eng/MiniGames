aff5825e874a1fa985492e42b77806afae30efbf
rssstudentjun-eng <rss.student.jun@gmail.com>
2026-09-17 21:01:00 +0400

    feat: add responsive hero section

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,5 +1,6 @@
 import '../styles/globals.scss';
 import { createHeader } from '../components/header/header';
+import { createHeroSection } from '../components/hero-section/hero-section.ts';
 
 const app = document.createElement('div');
 
@@ -7,6 +8,9 @@ app.id = 'app';
 
 const header = createHeader();
 const main = document.createElement('main');
+const heroSection = createHeroSection();
+
+main.append(heroSection);
 
 app.prepend(header, main);
 
