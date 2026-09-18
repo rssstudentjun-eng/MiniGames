77adf05007d303cfe13e007fa32ce46fd12f259b
rssstudentjun-eng <rss.student.jun@gmail.com>
2026-09-19 00:16:34 +0400

    feat: add base layout for game developers section

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,9 +1,9 @@
 import '../styles/globals.scss';
-import {createHeader} from '../components/header/header';
-import {createHeroSection} from '../components/hero-section/hero-section.ts';
-import {createCarouselSection} from '../components/carousel-section/carousel-section.ts';
-import {createLeadBoardSection} from '../components/leaderboard-table-section/leaderboard-table-section.ts';
-import {createGameDevelopersSection} from "../components/game-developers-section/game-developers-section.ts";
+import { createHeader } from '../components/header/header';
+import { createHeroSection } from '../components/hero-section/hero-section.ts';
+import { createCarouselSection } from '../components/carousel-section/carousel-section.ts';
+import { createLeadBoardSection } from '../components/leaderboard-table-section/leaderboard-table-section.ts';
+import { createGameDevelopersSection } from '../components/game-developers-section/game-developers-section.ts';
 
 const app = document.createElement('div');
 
