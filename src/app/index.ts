84f091d44e4617017f63866dcab313cbb670cc58
rssstudentjun-eng <rss.student.jun@gmail.com>
2026-09-19 00:15:24 +0400

    feat: add base layout for game developers section

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,8 +1,9 @@
 import '../styles/globals.scss';
-import { createHeader } from '../components/header/header';
-import { createHeroSection } from '../components/hero-section/hero-section.ts';
-import { createCarouselSection } from '../components/carousel-section/carousel-section.ts';
-import { createLeadBoardSection } from '../components/leaderboard-table-section/leaderboard-table-section.ts';
+import {createHeader} from '../components/header/header';
+import {createHeroSection} from '../components/hero-section/hero-section.ts';
+import {createCarouselSection} from '../components/carousel-section/carousel-section.ts';
+import {createLeadBoardSection} from '../components/leaderboard-table-section/leaderboard-table-section.ts';
+import {createGameDevelopersSection} from "../components/game-developers-section/game-developers-section.ts";
 
 const app = document.createElement('div');
 
@@ -13,8 +14,9 @@ const main = document.createElement('main');
 const heroSection = createHeroSection();
 const carouselSection = createCarouselSection();
 const leadBoardSection = createLeadBoardSection();
+const gameDevelopersSection = createGameDevelopersSection();
 
-main.append(heroSection, carouselSection, leadBoardSection);
+main.append(heroSection, carouselSection, leadBoardSection, gameDevelopersSection);
 
 app.prepend(header, main);
 
